66b924f009eac9427ae34ffbc32e78ddaab7a640
Aleksei Terentev <aleksei.terentev@NetCracker.com>
2021-02-14 23:52:31 +0400

    lesson1

--- lesson1/src/main/java/ru/geekbrains/alekseiterentev/Participant.java
+++ lesson1/src/main/java/ru/geekbrains/alekseiterentev/Participant.java
@@ -6,10 +6,34 @@ public class Participant {
         for (ObstacleCourseItem obstacleCourseItem : obstacleCourse) {
             if (obstacleCourseItem instanceof RunningTrack && this instanceof Runable) {
                 ((Runable) this).runThrough((RunningTrack) obstacleCourseItem);
+            } else {
+                if (this instanceof Cat) {
+                    System.out.println("Кот не смог пробежать");
+                }
+
+                if (this instanceof Human) {
+                    System.out.println("Человек не смог пробежать");
+                }
+
+                if (this instanceof Robot){
+                    System.out.println("Robot не смог пробежать");
+                }
             }
 
             if (obstacleCourseItem instanceof Barrier && this instanceof Jumpable) {
                 ((Jumpable) this).jumpOver((Barrier) obstacleCourseItem);
+            } else {
+                if (this instanceof Cat) {
+                    System.out.println("Кот не смог перепрыгнуть");
+                }
+
+                if (this instanceof Human) {
+                    System.out.println("Человек не смог перепрыгнуть");
+                }
+
+                if (this instanceof Robot){
+                    System.out.println("Robot не смог перепрыгнуть");
+                }
             }
         }
     }
